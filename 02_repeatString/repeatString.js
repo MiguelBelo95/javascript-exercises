4ce586921d567544841e05065c3505e052ea7ab7
MiguelBelo95 <belo_miguel@hotmail.com>
2022-11-22 15:41:06 +0000

    Exercise 01 and 02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, nb) {
+	
+	// Analyse input
+	if (nb < 0) {
+		return "ERROR";
+	} else if (nb === 0 || str === "") {
+		return "";
+	}
 
+	let substr = "";
+	while(nb > 0) {
+		substr += str.slice();
+		nb--;
+	}
+	return substr;
 };
 
 // Do not edit below this line
